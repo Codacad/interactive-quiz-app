681dfffccc1fcd6defb67181ed7e8a9b352fdb9b
Mohd Rizwan <iamrizwan40@gmail.com>
2023-11-02 10:03:31 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,7 @@ const quiz = require('./routes/quiz')
 
 const PORT = process.env.PORT || 5000
 const app = express()
-app.use(cors({
-  "origin": "*",
-  "methods": ["POST", "GET"],
-  "credentials": true
-}))
+app.use(cors())
 app.use(express.json())
 dbConnection()
 app.use(quiz)
